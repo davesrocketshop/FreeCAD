694b030cdd52738dadf8e92f132f71e6fac7afde
David Carter <dcarter@davidcarter.ca>
2025-10-29 17:37:00 -0400

    Remove linter error

--- src/App/Material.h
+++ src/App/Material.h
@@ -140,8 +140,9 @@ public:
     bool operator==(const Material& m) const
     {
         // clang-format off
-        if (!uuid.empty() && uuid == m.uuid)
+        if (!uuid.empty() && uuid == m.uuid) {
             return true;
+        }
         return shininess == m.shininess
             && transparency == m.transparency
             && ambientColor == m.ambientColor
